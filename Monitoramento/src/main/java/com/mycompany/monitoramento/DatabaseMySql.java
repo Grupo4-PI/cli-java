e51b032e72f6e2ee9e9d3a4eb40777e587a7e6aa
eduardaclx <eduarda.evangelista@sptech.school>
2022-11-13 21:47:46 -0300

    mysql working

--- Monitoramento/src/main/java/com/mycompany/monitoramento/DatabaseMySql.java
+++ Monitoramento/src/main/java/com/mycompany/monitoramento/DatabaseMySql.java
@@ -10,29 +10,41 @@ public class DatabaseMySql {
     private final JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSourceSql);
     private final SqlCommands sql = new SqlCommands();
 // ec2 config
-//    public DatabaseMySql() throws SQLException         
-//        dataSourceSql.setDriverClassName(
-//                "com.mysql.cj.jdbc.Driver");
-//        dataSourceSql.setUrl(
-//                "jdbc:mysql://172.17.0.2:3306/nocrash");
-//        dataSourceSql.setUsername("root");
-//        dataSourceSql.setPassword("urubu100");
-//    }
-
-    public DatabaseMySql() throws SQLException {
-        
+    public DatabaseMySql() throws SQLException  {       
         dataSourceSql.setDriverClassName(
                 "com.mysql.cj.jdbc.Driver");
         dataSourceSql.setUrl(
-                "jdbc:mysql://localhost:3306/nocrash");
+                "jdbc:mysql://172.17.0.2:3306/nocrash");
         dataSourceSql.setUsername("root");
-        dataSourceSql.setPassword("sptech");
+        dataSourceSql.setPassword("urubu100");
+    }
+        
+//local config
+//    public DatabaseMySql() throws SQLException {
+//        
+//        dataSourceSql.setDriverClassName(
+//                "com.mysql.cj.jdbc.Driver");
+//        dataSourceSql.setUrl(
+//                "jdbc:mysql://localhost:3306/nocrash");
+//        dataSourceSql.setUsername("root");
+//        dataSourceSql.setPassword("sptech");
+//    }
+    
+    public void insertHardware(String token) {
+         jdbcTemplate.update(sql.insertHardware(token));
     }
     
+    public void updateHardware(String token) {
+         jdbcTemplate.update(sql.updateHardware(token));
+    }
+
     public void inserirDados() {
          jdbcTemplate.update(sql.insertDados());
     }
-    public void inserirDiscos(Integer index) {
-         jdbcTemplate.update(sql.insertDisco(index));
+    public void inserirDisco(Integer indice) {
+         jdbcTemplate.update(sql.insertDisco(indice));
+    }
+    public void updateDisco(Integer indice) {
+         jdbcTemplate.update(sql.updateDisco(indice));
     }
 }
